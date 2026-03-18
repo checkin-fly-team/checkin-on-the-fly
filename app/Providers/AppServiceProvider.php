929c225b6fe10d78cfd07c49c3662ce3b35173df
Filipe Sousa <filipealsousa2005@hotmail.com>
2026-03-18 16:01:34 +0000

    force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // When running behind a TLS-terminating reverse proxy, force generated URLs to HTTPS.
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
